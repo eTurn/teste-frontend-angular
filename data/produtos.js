41a1982fdd72d5c85c57b26ca6710d9aedf4e4d0
Anderson Lopes <anderson.lopes@eturn.com.br>
2020-06-23 14:44:40 -0300

    inclusão de arquivos

---
+++ data/produtos.js
@@ -0,0 +1,367 @@
+[{
+    "id": 1,
+    "nome": "Casaco alfaiataria com bolsos laterais",
+    "categoria": "Casaco",
+    "notas": [
+      {
+        nota: "5"
+      },
+      {
+        nota: "4"
+      },
+      {
+        nota: "2"
+      },
+      {
+        nota: "1"
+      },
+      {
+        nota: "3"
+      }
+    ],
+    "imagens": [
+      {
+          nome: "L1-1.jpg",
+          destacada: false,
+      },
+      {
+          nome: "L1-2.jpg",
+          destacada: true,
+      }
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+  }, {
+    "id": 2,
+    "nome": "Blusa com pregas e gola arredondada",
+    "categoria": "Blusa",
+    "notas": [
+      {
+          nota: 4,
+      },
+      {
+          nota: 5,
+      },
+      {
+          nota:1,
+      },
+      {
+          nota: 5,
+      }
+    ],
+    "imagens": [
+      {
+          nome: "L2-1.jpg",
+          destacada: false
+      },
+      {
+          nome: "L2-2.jpg",
+          destacada: true,
+      },
+      {
+          nome: "L2-3.jpg",
+          destacada:false
+      }
+    ],
+    "precoMedio": 2,
+    "disponivel": true
+  }, {
+    "id": 3,
+    "nome":"Vestido midi tubinho",
+    "categoria": "Vestido",
+    "notas": [
+      {
+          nota: 4,
+      },
+    ],
+    "imagens": [
+      {
+          nome: "L3-1.jpg",
+          destacada: true,
+      },
+    ],
+    "precoMedio": 4,
+    "disponivel": false
+  }, {
+    "id": 4,
+    "nome": "Calça social alfaiataria com bolsos",
+    "categoria": "Calça",
+    "notas": [
+      {
+          nota:5,
+      },
+      {
+          nota:3
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L4-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": true
+  }, {
+    "id": 5,
+    "nome": "Blusa de algodão com estampa de sorvete",
+    "categoria": "Blusa",
+    "notas": [
+      {
+          nota:4,
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L5-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 1,
+    "disponivel": false
+  },  {
+    "id": 6,
+    "nome": "Body preto básico",
+    "categoria": "Body",
+    "notas": [
+      {
+          nota:1,
+      },
+      {
+          nota: 4,
+      },
+      {
+          nota: 2,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:5
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L6-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+  }, {
+    "id": 7,
+    "nome": "Body cavado com corte",
+    "categoria": "Body",
+    "notas": [
+      {
+          nota:1,
+      },
+      {
+          nota: 4,
+      },
+      {
+          nota: 2,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:5
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L7-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+  }, {
+    "id": 8,
+    "nome": "Blusa ciganinha branca com elástico",
+    "categoria": "Blusa",
+    "notas": [
+      {
+          nota:4,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:4
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L8-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 2,
+    "disponivel": false
+  },{
+    "id": 9,
+    "nome": "Casaco alfaiataria slim com bolsos",
+    "categoria": "Casaco",
+    "notas": [
+      {
+          nota:5,
+      },
+      {
+          nota:5
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L9-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+  },  
+  {
+    "id": 10,
+    "nome": "Vestido branco com detalhes em renda",
+    "categoria": "Vestido",
+    "notas": [
+      {
+          nota:1,
+      },
+      {
+          nota: 4,
+      },
+      {
+          nota: 2,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:5
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L10-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 2,
+    "disponivel": false
+  }, {
+    "id": 11,
+    "nome": "Blusa branca com corte",
+    "categoria": "Blusa",
+    "notas": [
+      {
+          nota:5,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:4,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:5
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L11-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 1,
+    "disponivel": true
+  }, {
+    "id": 12,
+    "nome": "Calça jeans com rasgos e detalhes",
+    "categoria": "Calça",
+    "notas": [
+    ],
+    "imagens": [
+        {
+            nome: "L12-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+  }, {
+    "id": 9,
+    "nome": null,
+    "categoria": null,
+    "notas": [
+      {
+          nota:3,
+      },
+      {
+          nota:3,
+      },
+      {
+          nota:5,
+      },
+      {
+          nota:2,
+      },
+      {
+          nota:3
+      },
+      {
+          nota:3
+      }
+    ],
+    "imagens": [
+        {
+            nome: "L12-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+  }]
